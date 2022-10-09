83e03e8937c952de4f916a89bdc529351e32d31f
lesson1 <b2201980@photon.chitose.ac.jp>
2022-10-09 19:03:04 +0900

    calculateボタンを押すと、ResulutViewControllerに画面遷移するように変更した

---
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by 平川恵輔 on 2022/10/09.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var adviceLabel: UILabel!
+    @IBOutlet weak var bmiLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+
+  
+
+     @IBAction func recalculatePressed(_ sender: UIButton) {
+     }
+     
+
+}
