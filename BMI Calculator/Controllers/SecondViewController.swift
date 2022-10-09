71d2feb1bf4c83c7e094ffb68c569dc2caf885ad
lesson1 <b2201980@photon.chitose.ac.jp>
2022-10-09 18:36:17 +0900

    2つ目の画面をコントロールするファイルを作成した

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by 平川恵輔 on 2022/10/09.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController{
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
