c2c5b5b7249f5256827d91c5679390fd988e5503
lesson1 <b2201980@photon.chitose.ac.jp>
2022-10-09 19:20:10 +0900

    recalculateボタンを押すと、前の画面(計算画面)に戻るように変更した

--- BMI Calculator/Controllers/ResultViewController.swift
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -29,6 +29,7 @@ class ResultViewController: UIViewController {
   
 
      @IBAction func recalculatePressed(_ sender: UIButton) {
+         self.dismiss(animated: true, completion: nil)
      }
      
 
