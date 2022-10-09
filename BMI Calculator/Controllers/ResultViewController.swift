4102f8d8e1b3eb8e4f3ab9dec023e41f79bb93d2
lesson1 <b2201980@photon.chitose.ac.jp>
2022-10-09 19:17:55 +0900

    calculateボタンを押すと、次の画面で計算したBMIが表示されるように変更した

--- BMI Calculator/Controllers/ResultViewController.swift
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -9,15 +9,20 @@
 import UIKit
 
 class ResultViewController: UIViewController {
+    
+    var bmiValue: String?
+
 
     @IBOutlet weak var adviceLabel: UILabel!
+    
+    
     @IBOutlet weak var bmiLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        bmiLabel.text = bmiValue
     }
     
 
